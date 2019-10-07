53fcbca7dc07c7cc65c883dbe2ef9594ec95fa6a
Erik Målsäter <erik.malsater92@gmail.com>
2019-10-07 15:53:47 +0200

    creates foundation for CategoryItem

--- app/src/containers/category/Category.js
+++ app/src/containers/category/Category.js
@@ -1,27 +1,39 @@
 import React, { Component } from 'react';
 import { getCategoryList } from './state/category';
 import './Category.scss';
+import CategoryItem from '../categoryItem/CategoryItem';
 
 export default class Category extends Component {
-  state = { category: {} };
+  state = { 
+    category: {},
+    isLoading: true
+  };
   componentDidMount() {
     const loadedData = getCategoryList();
-
     this.setState({ category: loadedData });
+    this.setState({ isLoading: false });
   }
+
+  /*{this.state.category.category.products.map((item) => {
+    //return <CategoryItem name="name"/>
+    return <p>hej</p>
+  })}*/
 
   render() {
     /*
      * You can get the category data from the category state:
      * const { category } = this.state;
      */
+    if(this.state.isLoading) {
+      return <p>loading...</p>
+      
+    }
     return (
-      <div className="hello-world">
-        Hello world!
-        <span role="img" aria-label="Wave">
-          👋🏻
-        </span>
-      </div>
+      <ul>
+        {this.state.category.products.map((item) => {
+          return <CategoryItem name={item.name}/>
+        })}
+      </ul>
     );
   }
 }
